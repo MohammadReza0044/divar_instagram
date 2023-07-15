942d13b12166465ff516dc7479921e8ce9b3e389
MohammadReza0044 <mohammadreza.shayegan@gmail.com>
2023-07-15 15:54:31 +0430

    initiat product app

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 from decouple import config
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "user",
+    "product",
 ]
 
 MIDDLEWARE = [
@@ -144,4 +146,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     )
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
 }
---
+++ product/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+from user.models import User
+
+
+class Product(models.Model):
+    CATEGORY_TYPE_CHOICES = (
+        ("موبایل", "موبایل"),
+        ("تبلت ", "تبلت "),
+    )
+    DEVICE_STATUS_TYPE_CHOICES = (
+        ("در حد نو", "در حد نو"),
+        (" کار کرده", "کار کرده "),
+        (" نیاز به تعمیر ", "نیاز به تعمیر  "),
+    )
+    ORIGINALITY_TYPE_CHOICES = (
+        ("اصل", "اصل"),
+        ("غیر اصل ", "غیر اصل "),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    add_title = models.CharField(max_length=255)
+    category = models.CharField(max_length=50, choices=CATEGORY_TYPE_CHOICES)
+    city = models.CharField(max_length=100)
+    Neighbourhood = models.CharField(max_length=100)
+    brand_name = models.CharField(max_length=100)
+    device_model = models.CharField(max_length=100)
+    device_status = models.CharField(max_length=50, choices=DEVICE_STATUS_TYPE_CHOICES)
+    brand_originality = models.CharField(
+        max_length=50, choices=ORIGINALITY_TYPE_CHOICES
+    )
+    price = models.IntegerField()
+    description = models.TextField()
+    created_at = models.DateTimeField(db_index=True, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "Product"
+
+    def __str__(self):
+        return self.add_title
+
+
+class ProductPhoto(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="images"
+    )
+    image = models.ImageField(upload_to="Product/images", null=True)
+
+    class Meta:
+        db_table = "Product Photos"
