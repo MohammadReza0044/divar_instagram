c22b2f617944f604d100132f74997906bc6b445e
MohammadReza0044 <mohammadreza.shayegan@gmail.com>
2023-07-22 20:40:07 +0430

    product Test are done

--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,73 @@
-from django.test import TestCase
+import io
 
-# Create your tests here.
+from django.urls import reverse
+from PIL import Image
+from rest_framework.test import APITestCase
+
+from user.models import User
+
+from .models import Product
+
+
+class ProductViewSetTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(first_name="testuser")
+        self.object = Product.objects.create(
+            add_title="Test Object 1", price=1000, user=self.user
+        )
+
+    def generate_photo_file(self):
+        file = io.BytesIO()
+        image = Image.new("RGBA", size=(100, 100), color=(155, 0, 0))
+        image.save(file, "png")
+        file.name = "test.png"
+        file.seek(0)
+        return file
+
+    def test_create_object(self):
+        photo_file = self.generate_photo_file()
+        url = reverse("product:product-list")
+        data = {
+            "user": self.user.pk,
+            "add_title": "Test Object 2",
+            "category": "موبایل",
+            "city": "Test Object",
+            "Neighbourhood": "Test Object",
+            "brand_name": "Test Object",
+            "device_model": "Test Object",
+            "device_status": "در حد نو",
+            "brand_originality": "اصل",
+            "price": 1000,
+            "description": "Test Object",
+            "uploaded_images": photo_file,
+        }
+
+        response = self.client.post(url, data, format="multipart")
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Product.objects.count(), 2)
+
+    def test_patch_object(self):
+        photo_file = self.generate_photo_file()
+        url = reverse("product:product-detail", args=[self.object.pk])
+        data = {
+            "price": 2000,
+            "update_photos": photo_file,
+        }
+
+        response = self.client.patch(url, data, format="multipart")
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_photo(self):
+        url = reverse("product:product-detail", args=[self.object.pk])
+        data = {
+            "delete_photos": 1,
+        }
+
+        response = self.client.patch(url, data, format="multipart")
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_object(self):
+        url = reverse("product:product-detail", args=[self.object.pk])
+
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, 204)
